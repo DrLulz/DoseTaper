3bd0f98aa08c6e9dd941612f471b99611cae8872
Drlulz <drlulzmd@gmail.com>
2016-08-14 13:53:01 -0400

    initial commit

---
+++ sectors/__init__.py
@@ -0,0 +1,86 @@
+from flask import Flask, render_template
+from utils import get_instance_folder_path
+#from flask_security import current_user, user_registered, SQLAlchemyUserDatastore
+#from cache import cache
+#from mail import mail
+#from session import sess
+#from csrf import csrf
+#from config import configure_app, make_celery
+#from data.models import db, User, Role
+#import errors
+
+from sectors.main.controllers import main
+#from project.admin.controllers import admin
+#from project.user.controllers import user
+#from project.results.controllers import results
+#from project.calendar.controllers import calendar
+#from project.tutorials.controllers import tutorials
+#from project.disclaimer.controllers import disclaimer
+
+
+app = Flask(__name__,
+				instance_path=get_instance_folder_path(),
+				instance_relative_config=True,
+				template_folder='templates')
+
+app.register_blueprint(main, url_prefix='/')
+
+'''
+configure_app(app)
+cache.init_app(app)
+sess.init_app(app)
+db.init_app(app)
+mail.init_app(app)
+csrf.init_app(app)
+errors.init_app(app)
+
+app.jinja_env.add_extension('jinja2.ext.loopcontrols')
+
+app.register_blueprint(main, url_prefix='/')
+app.register_blueprint(admin, url_prefix='/admin')
+app.register_blueprint(user, url_prefix='/user')
+app.register_blueprint(results, url_prefix='/results')
+app.register_blueprint(calendar, url_prefix='/calendar')
+app.register_blueprint(tutorials, url_prefix='/tutorials')
+app.register_blueprint(disclaimer, url_prefix='/disclaimer')
+
+celery = make_celery(app)
+
+
+@app.context_processor
+def inject_user():
+	return dict(user=current_user)
+
+#@app.errorhandler(DatabaseError)
+#def special_exception_handler(error):
+#    return 'Database connection failed', 500
+
+@app.template_filter()
+def datefilter(value, format='%m/%d/%y'):
+	import datetime as dt
+	date = dt.datetime.strptime(value, '%Y-%m-%d')
+	return date.strftime(format)
+
+@app.template_filter()
+def date_slash(value, format='%m/%d/%Y'):
+	import datetime as dt
+	date = dt.datetime.strptime(value, '%Y-%m-%d')
+	return date.strftime(format)
+
+@app.template_filter()
+def title_id(title):
+	import re
+	return re.sub(' ', '_', title)
+
+app.jinja_env.filters['datefilter'] = datefilter
+app.jinja_env.filters['date_slash'] = date_slash
+app.jinja_env.filters['title_id']   = title_id
+
+
+@user_registered.connect_via(app)
+def user_registered_sighandler(app, user, confirm_token):
+	user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+	default_role = user_datastore.find_role("user")
+	user_datastore.add_role_to_user(user, default_role)
+	db.session.commit()
+'''
---
+++ sectors/static/js/custom.js
@@ -0,0 +1,120 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        easing: 'linear',
+        anchors: ['intro00', 'intro01'],
+        //sectionsColor: ['rgba(255, 255, 255, 0)', '#4BBFC3'],
+        //css3: false,
+        //autoScrolling: false,
+        //verticalCentered: true,
+        //fitToSection: true,
+        onLeave: function(index, nextIndex, direction) {
+            // after leaving Introduction
+            if (index == 1 && direction == 'down') {
+                $(".navbar .container").animate({ height: 40 }, 100, 'linear');
+                $(".navbar").animate({
+                    backgroundColor: jQuery.Color("#ddd8ce").transition("transparent", 0.4),
+                    //backgroundColor: jQuery.Color("#F8F2ED").transition("transparent", 0),
+                    borderBottomColor: jQuery.Color("#ddd8ce").transition("transparent", 0)
+                }, 400, 'easeInOutCubic');
+
+                $(".navbar").animate({ backgroundColor: "#F8F2ED" }, 1000);
+
+                $(".nav-item .nav-link, .nav-item .nav-link:hover").animate({ color: jQuery.Color("#555") }, 400, 'linear');
+                $(".hamburger-icon .line").animate({
+                    backgroundColor: jQuery.Color("#555").transition("transparent", 0)
+                }, 400, 'easeInOutCubic');
+                $(".nav-item .nav-link").toggleClass('nav-dark');
+            }
+            // going to Introduction
+            if (index == 2 && direction == 'up') {
+                $(".navbar .container").animate({ height: 98 }, 100, 'linear');
+                $(".navbar").animate({
+                    backgroundColor: jQuery.Color("#ddd8ce").transition("transparent", 1),
+                    borderBottomColor: jQuery.Color("#ddd8ce").transition("transparent", 1)
+                }, 400, 'linear');
+                $(".nav-item .nav-link, .nav-item .nav-link:hover").animate({ color: jQuery.Color("#fff") }, 400, 'linear');
+                $(".hamburger-icon .line").animate({
+                    backgroundColor: jQuery.Color("#ecf0f1").transition("transparent", 0)
+                }, 400, 'easeInOutCubic');
+                $(".nav-item .nav-link").toggleClass('nav-dark');
+            }
+        }
+    });
+
+    var ham_icon = $('.hamburger-icon');
+    ham_icon.click(function() {
+        $(".overlay").fadeToggle(200);
+        ham_icon.toggleClass('active');
+        $(window).on("resize", resetMenu);
+        var color = $('.hamburger-icon .line').css("backgroundColor");
+        resetHam();
+        return false;
+    });
+
+    $('.overlay').on('click', function() {
+        $(".overlay").fadeToggle(200);
+        ham_icon.toggleClass('active');
+    });
+
+    $('.menu a').on('click', function() {
+        $(".overlay").fadeToggle(200);
+        ham_icon.toggleClass('active');
+    });
+    var resetMenu = function() {
+        if (($('.overlay').is(":visible")) && ($(window).width() > 767)) {
+            $(".overlay").fadeToggle(200);
+            ham_icon.toggleClass('active');
+            $(window).off("resize", resetMenu);
+            resetHam();
+        }
+    };
+    var resetHam = function() {
+        if (ham_icon.hasClass('active')) {
+            $('.hamburger-icon .line').css('background-color', '#ecf0f1');
+        } else if ($('.nav-item .nav-link').hasClass('nav-dark')) {
+            $('.hamburger-icon .line').css('background-color', '#555');
+        } else {
+            $('.hamburger-icon .line').css('background-color', '#ecf0f1');
+        }
+    };
+
+    $("img.lazy").lazyload({
+        effect: "fadeIn",
+        container: $("#section01")
+    });
+});
+
+/* flexibility - https://github.com/jonathantneal/flexibility
+---------------------------------------------------------------------------*/
+/* function supportsFlexBox() {
+    var test = document.createElement('test');
+
+    test.style.display = 'flex';
+
+    return test.style.display === 'flex';
+}
+
+if (supportsFlexBox()) {
+    // Modern Flexbox is supported
+} else {
+    flexibility(document.documentElement);
+}
+
+var onresizeTimeout;
+
+window.onresize = onresize;
+
+function onresize() {
+    window.onresize = null;
+
+    if (!onresizeTimeout) {
+        onresizeTimeout = setTimeout(function() {
+            onresizeTimeout = null;
+
+            flexibility(container);
+
+            window.onresize = onresize;
+        }, 1000 / 60);
+    }
+}
+ */
