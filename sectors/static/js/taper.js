8f39b3352dda14c6ebfc21ea0559999bbc293422
pguilford <drlulz@MacBook-Pro.local>
2017-04-30 08:53:00 -0400

    test

--- sectors/static/js/taper.js
+++ sectors/static/js/taper.js
@@ -165,7 +165,7 @@ $.fn.autoGrowInput = function(o) {
             var iOS = deviceAgent.match(/(iPad|iPhone|iPod)/i);
             if (iOS) {
                 console.log('iOS');
-                $('tester').remove();
+                //$('tester').remove();
                 //testSubject.insertAfter(input);
                 //window.dispatchEvent(new Event('resize'));
                 //check();
