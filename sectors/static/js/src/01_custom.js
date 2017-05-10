1d50016d855f9fc1bc019aaa8f5f6f66aaef5ea3
pguilford <drlulz@MacBook-Pro.local>
2017-05-10 08:10:23 -0400

    ham icon mobile

--- sectors/static/js/src/01_custom.js
+++ sectors/static/js/src/01_custom.js
@@ -31,7 +31,7 @@ ham.add(overlay).click(function(e) {
     ham.toggleClass('active');
     $('.ham-icon > .line').toggleClass('line-active');
     if (mobile && !ham.hasClass('active')) {
-        $('line-1, line-3').css({'-webkit-transform': 'translateY(0px)', '-moz-transform': 'translateY(0px)', '-ms-transform': 'translateY(0px)', '-o-transform': 'translateY(0px)', 'transform': 'translateY(0px)'});
+        $('.line-1, .line-3').css({'-webkit-transform': 'translateY(0px)', '-moz-transform': 'translateY(0px)', '-ms-transform': 'translateY(0px)', '-o-transform': 'translateY(0px)', 'transform': 'translateY(0px)'});
     }
     $(window).on('resize', resetMenu);
 });
