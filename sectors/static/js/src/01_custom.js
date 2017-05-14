5de2d50e52c1625e22796b3fc2c347504b4bc048
pguilford <drlulz@MacBook-Pro.local>
2017-05-14 05:38:40 -0400

    table

--- sectors/static/js/src/01_custom.js
+++ sectors/static/js/src/01_custom.js
@@ -175,3 +175,47 @@ if (isIos() && isRunningStandalone()) {
         }, false);
     }
 }
+
+/* remove hover on mobile results page
+---------------------------------------------------------------------------*/
+if (mobile && $('#results').length) {
+    console.log('remove class')
+    $('table').removeClass('table-hover');
+}
+
+/* utils
+---------------------------------------------------------------------------*/
+
+// http://www.inabrains.com/tooltip/guide.html#api
+/* $('#input_date').LiteTooltip({
+    title: 
+    '<div class="l-tip">' +
+    'Start Date' +
+    '</div>'
+});  */
+
+// http://ned.im/noty/options.html
+var n = new Noty({
+    type: 'warning', // alert, success, error, warning, info
+    layout: 'topCenter', // top, topLeft, topCenter, topRight, center, centerLeft, centerRight, bottom, bottomLeft, bottomCenter, bottomRight
+    theme: 'mint',
+    text: 'Notification', // HTML or string
+    timeout: 2000, // false, 1000, 3000, 3500, etc.
+    progressBar: true,
+    closeWith: ['click', 'button'],
+    animation: {
+        open: 'noty_effects_open',
+        close: 'noty_effects_close'
+    },
+    id: false,
+    force: false,
+    killer: false,
+    queue: 'global',
+    container: false,
+    buttons: [],
+    titleCount: {
+        conditions: []
+    },
+    modal: true
+});
+//n.show()
