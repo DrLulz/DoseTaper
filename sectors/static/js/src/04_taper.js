f88f344e7f4a70e0d3276835fb610877abf16acb
pguilford <drlulz@MacBook-Pro.local>
2017-05-14 05:15:24 -0400

    results mobile

--- sectors/static/js/src/04_taper.js
+++ sectors/static/js/src/04_taper.js
@@ -34,43 +34,6 @@ var n = new Noty({
 //n.show()
 
 
-/**
- * Created by Kupletsky Sergey on 05.11.14.
- *
- * Material Design Responsive Table
- * Tested on Win8.1 with browsers: Chrome 37, Firefox 32, Opera 25, IE 11, Safari 5.1.7
- * You can use this table in Bootstrap (v3) projects. Material Design Responsive Table CSS-style will override basic bootstrap style.
- * JS used only for table constructor: you don't need it in your project
- */
-
-$(document).ready(function() {
-
-    var table = $('#table');
-
-    // Table bordered
-    $('#table-bordered').change(function() {
-        var value = $( this ).val();
-        table.removeClass('table-bordered').addClass(value);
-    });
-
-    // Table striped
-    $('#table-striped').change(function() {
-        var value = $( this ).val();
-        table.removeClass('table-striped').addClass(value);
-    });
-  
-    // Table hover
-    $('#table-hover').change(function() {
-        var value = $( this ).val();
-        table.removeClass('table-hover').addClass(value);
-    });
-
-    // Table color
-    $('#table-color').change(function() {
-        var value = $(this).val();
-        table.removeClass(/^table-mc-/).addClass(value);
-    });
-});
 
 // jQuery’s hasClass and removeClass on steroids
 // by Nikita Vasilyev
